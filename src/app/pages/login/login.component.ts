29283807871561f485d8198d5bde388b09ca5643
Glains <lukas-kannenberg@web.de>
2017-05-02 16:17:56 +0200

    feat(login): add basic template and markup

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output, ViewEncapsulation } from "@angular/core";
+import { AbstractControl, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss'],
+    encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    public loginForm: FormGroup;
+    public usernameCtrl: AbstractControl;
+    public passwordCtrl: AbstractControl;
 
-  ngOnInit() {
-  }
+    /**
+     * Whether authentication is currently is progress.
+     */
+    private _loading: boolean = false;
+
+    constructor(private _fb: FormBuilder) {
+        this.initializeLoginForm();
+    }
+
+    ngOnInit() {
+
+    }
+
+    /**
+     * Initializes the login form.
+     */
+    private initializeLoginForm() {
+        this.loginForm = this._fb.group({
+            username: ['', Validators.required],
+            password: ['', Validators.compose([
+                Validators.required
+            ])]
+        });
+        this.usernameCtrl = this.loginForm.get('username');
+        this.passwordCtrl = this.loginForm.get('password');
+    }
+
+    /**
+     * Attempts to login the user with the specified credentials. Delegated to the login service.
+     */
+    login() {
+        // TODO
+    }
+
+    /**
+     * Redirects to the registration form foe a new user.
+     */
+    redirectToRegisterPage() {
+        // TODO
+    }
+
+    /**
+     * Redirects to the recover password page.
+     */
+    redirectToRecoverPasswordPage() {
+        // TODO
+    }
 
 }
